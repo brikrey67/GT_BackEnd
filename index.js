0ac35ba9f457f8c1e44dc97db4ee1ba4df5c1169
Temicka Brown <mekapleio@yahoo.com>
2018-01-29 13:14:47 -0500

    updated

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // require("dotenv").config();
 
-const express = require("express");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const methodOverride = require("method-override");
-const mongoose = require("mongoose");
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+const mongoose = require('mongoose')
 
-const routes = require("./config/routes");
-const app = express();
+const routes = require('./config/routes')
+const app = express()
 
 // const flash = require("connect-flash");
 // const morgan = require("morgan");
@@ -17,9 +17,9 @@ const app = express();
 
 // app.use(morgan("dev"));
 // app.use(cookieParser());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(methodOverride("_method"));
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(methodOverride('_method'))
 
 // app.use(session({ secret: "WDI-GENERAL-ASSEMBLY-EXPRESS" }));
 // app.use(passport.initialize());
@@ -38,11 +38,11 @@ app.use(methodOverride("_method"));
 //   next();
 // });
 
-app.use(cors());
-app.use(routes);
+app.use(cors())
+app.use(routes)
 
-app.set("port", process.env.PORT || 3001);
+app.set('port', process.env.PORT || 3001)
 
-app.listen(app.get("port"), () => {
-  console.log(`✅ PORT: ${app.get("port")} 🌟`);
-});
+app.listen(app.get('port'), () => {
+  console.log(`✅ PORT: ${app.get('port')} 🌟`)
+})
