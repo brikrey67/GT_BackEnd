286d60b85feea022541c3820e11ded6fea5bc456
neechie <neechieg@gmail.com>
2018-01-29 12:30:15 -0500

    "syncing project with teammates"

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 // require("dotenv").config();
 
-const express = require("express");
-const cors = require("cors");
-const app = express();
-const mongoose = require("mongoose");
-const flash = require("connect-flash");
-const morgan = require("morgan");
-var cookieParser = require("cookie-parser");
-var bodyParser = require("body-parser");
-const methodOverride = require("method-override");
-var session = require("express-session");
+const express = require("express")
+const cors = require("cors")
+const app = express()
+const mongoose = require("mongoose")
+const flash = require("connect-flash"
+const morgan = require("morgan")
+var cookieParser = require("cookie-parser")
+var bodyParser = require("body-parser")
+const methodOverride = require("method-override")
+var session = require("express-session")
 
-app.use(morgan("dev"));
-app.use(cookieParser());
+app.use(morgan("dev"))
+app.use(cookieParser())
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride("_method"));
 
-app.use(session({ secret: "WDI-GENERAL-ASSEMBLY-EXPRESS" }));
+app.use(session({ secret: "WDI-GENERAL-ASSEMBLY-EXPRESS" }))
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
