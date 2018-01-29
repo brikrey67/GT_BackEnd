e030fffabd8c4718bc7dcca0db579cf0750ffdcb
Brian Reynolds <brikrey@gmail.com>
2018-01-29 12:42:21 -0500

    refactored index.js

--- index.js
+++ index.js
@@ -2,35 +2,39 @@
 
 const express = require("express");
 const cors = require("cors");
-const app = express();
-const mongoose = require("mongoose");
-const flash = require("connect-flash");
-const morgan = require("morgan");
-var cookieParser = require("cookie-parser");
-var bodyParser = require("body-parser");
+const bodyParser = require("body-parser");
 const methodOverride = require("method-override");
-var session = require("express-session");
-
-app.use(morgan("dev"));
-app.use(cookieParser());
-app.use(cors());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(methodOverride("_method"));
-
-app.use(session({ secret: "WDI-GENERAL-ASSEMBLY-EXPRESS" }));
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(flash());
-
-require("./config/passport")(passport);
-
-app.use(function(req, res, next) {
-  global.currentUser = req.user;
-  res.locals.currentUser = req.user;
-  next();
-});
+const mongoose = require("mongoose");
 
 const routes = require("./config/routes");
+const app = express();
+
+// const flash = require("connect-flash");
+// const morgan = require("morgan");
+// var cookieParser = require("cookie-parser");
+
+// var session = require("express-session");
+
+// app.use(morgan("dev"));
+// app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(methodOverride("_method"));
+
+// app.use(session({ secret: "WDI-GENERAL-ASSEMBLY-EXPRESS" }));
+// app.use(passport.initialize());
+// app.use(passport.session());
+// app.use(flash());
+
+// require("./config/passport")(passport);
+
+// app.use(function(req, res, next) {
+//   global.currentUser = req.user;
+//   res.locals.currentUser = req.user;
+//   next();
+// });
+
+app.use(cors());
 app.use(routes);
 
 app.set("port", process.env.PORT || 3001);
