dec46c2f2aa6d724a08222280373c0c0df3c92d4
neechie <neechieg@gmail.com>
2018-01-30 14:27:52 -0500

    comment our cat routes so that localhost and react sites still render

--- config/routes.js
+++ config/routes.js
@@ -7,7 +7,7 @@ var methodOverride = require('method-override')
 
 var todoController = require('../controllers/todo')
 
-var catController = require('./controllers/cat')
+// var catController = require('./controllers/cat')
 
 // TODO ROUTES ******************************
 router.route('/todo').get(todoController.todoGetAll)
@@ -25,19 +25,19 @@ router.route('/todo/:id').delete(todoController.todoDelete)
 router.route('/todo/:id').put(todoController.todoPut)
 
 // CATEGORY ROUTES ******************************
-router.route('/cat').get(catController.catGetAll)
-// list all categories
+// router.route('/cat').get(catController.catGetAll)
+// // list all categories
 
-router.route('/cat/:id').get(catController.catGetOne)
-// show selected category detail
+// router.route('/cat/:id').get(catController.catGetOne)
+// // show selected category detail
 
-router.route('/cat').post(catController.catPost)
-// add new category -- May not need as we will include a selector for user --
+// router.route('/cat').post(catController.catPost)
+// // add new category -- May not need as we will include a selector for user --
 
-router.route('/cat/:id').delete(catController.catDelete)
-// may not need as our app setup would be best without it
+// router.route('/cat/:id').delete(catController.catDelete)
+// // may not need as our app setup would be best without it
 
-router.route('/cat/:id').put(catController.catPut)
+// router.route('/cat/:id').put(catController.catPut)
 // may not need as our app setup would be best without it
 
 module.exports = router
